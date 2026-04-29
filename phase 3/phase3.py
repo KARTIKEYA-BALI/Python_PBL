28e0bd95fcfed986ef262a73da89a46bf366d70c
KARTIKEYA-BALI <balikartikeya@gmail.com>
2026-04-30 01:58:35 +0530

    WE HAVE DONE OUR PHASE 3

---
+++ phase 3/phase3.py
@@ -0,0 +1,53 @@
+import joblib
+import joblib
+import nltk
+import regex as re
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+
+
+
+def listing(s):
+
+    if type(s)==list:
+        return s
+    
+
+    text = re.sub(r'[^a-zA-Z\s]', '', s)
+    stp_wrd = set(stopwords.words("english"))
+    tokens = word_tokenize(text.lower())
+    ps = PorterStemmer()
+    
+    return [ps.stem(i) for i in tokens if i not in stp_wrd]
+
+tfidf = joblib.load(r"C:\Users\balih\Desktop\AI ML lab work\Python_PBL\phase 3\tfidf.pkl")
+model = joblib.load(r"C:\Users\balih\Desktop\AI ML lab work\Python_PBL\phase 3\model.pkl")
+
+
+s = input("enter your email: ")
+
+
+x = tfidf.transform([s])
+k = model.predict(x)
+print(k)
+if k[0] == 1:
+    print("the email might be spam")
+else:
+    print("the email might not be spam")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
